4441efddbf13698e589b8f6fb516eb7782e8570a
scientistkev <kpmcpherson@gmail.com>
2020-10-01 14:51:18 -0400

    adds to logloss.py

--- kevin/test_metrics/logloss.py
+++ kevin/test_metrics/logloss.py
@@ -1,2 +1,17 @@
 import scipy as sp
 
+def logloss(act, pred):
+    """ Vectorised computation of logloss - compliments of 
+    user Chris McGinlay """
+    
+    #cap in official Kaggle implementation, 
+    #per forums/t/1576/r-code-for-logloss
+    epsilon = 1e-15
+    pred = sp.maximum(epsilon, pred)
+    pred = sp.minimum(1-epsilon, pred)
+    
+    #compute logloss function (vectorised)
+    ll = sum(   act*sp.log(pred) + 
+                sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
+    ll = ll * -1.0/len(act)
+    return ll
